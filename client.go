c2e4b44c191973a1ae46add243ffa6aa28a7dff3
Ichinose Shogo <shogo82148@gmail.com>
2017-02-02 12:50:24 +0900

    add websocket support

--- client.go
+++ client.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptrace"
+	"net/url"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 type Client struct {
@@ -16,8 +19,20 @@ type Result struct {
 	Delay  time.Duration
 }
 
-func (c *Client) Get(url string) (Result, error) {
-	req, err := http.NewRequest("GET", url, nil)
+func (c *Client) Get(uri string) (Result, error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return Result{}, err
+	}
+
+	if u.Scheme == "ws" || u.Scheme == "wss" {
+		return c.getWebsocket(uri)
+	}
+	return c.getHTTP(uri)
+}
+
+func (c *Client) getHTTP(uri string) (Result, error) {
+	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		return Result{}, err
 	}
@@ -51,6 +66,35 @@ func (c *Client) Get(url string) (Result, error) {
 	ntpTime := time.Time(result.SendTime)
 	delay := end.Sub(start)
 	offset := start.Sub(ntpTime) + delay/2
+
+	return Result{
+		Delay:  delay,
+		Offset: offset,
+	}, nil
+}
+
+func (c *Client) getWebsocket(uri string) (Result, error) {
+	conn, _, err := websocket.DefaultDialer.Dial(uri, nil)
+	if err != nil {
+		return Result{}, nil
+	}
+
+	// Send the request
+	err = conn.WriteJSON(Timestamp(time.Now()))
+	if err != nil {
+		return Result{}, nil
+	}
+
+	// Parse the response
+	var response Response
+	err = conn.ReadJSON(&response)
+	if err != nil {
+		return Result{}, nil
+	}
+	start := time.Time(response.InitiateTime)
+	end := time.Now()
+	delay := end.Sub(start)
+	offset := start.Sub(time.Time(response.SendTime)) + delay/2
 
 	return Result{
 		Delay:  delay,
--- server.go
+++ server.go
@@ -1,15 +1,31 @@
 package webntp
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
+var defaultUpgrader = &websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 type Server struct {
+	Upgrader *websocket.Upgrader
 }
 
 func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if websocket.IsWebSocketUpgrade(req) {
+		s.handleWebsocket(rw, req)
+		return
+	}
+
 	res := &Response{
 		ID:           req.Host,
 		InitiateTime: zeroEpochTime,
@@ -22,4 +38,56 @@ func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
 	enc := json.NewEncoder(rw)
 	enc.Encode(res)
+}
+
+func (s *Server) handleWebsocket(rw http.ResponseWriter, req *http.Request) {
+	upgrader := s.Upgrader
+	if upgrader == nil {
+		upgrader = defaultUpgrader
+	}
+	conn, err := upgrader.Upgrade(rw, req, nil)
+	if err != nil {
+		log.Println("upgrade error: ", err)
+		return
+	}
+
+	for {
+		err := s.handleWebsocketConn(conn, req.Host)
+		if _, ok := err.(*websocket.CloseError); ok {
+			return
+		}
+		if err != nil {
+			log.Println("websocket error: ", err)
+			return
+		}
+	}
+}
+
+func (s *Server) handleWebsocketConn(conn *websocket.Conn, host string) error {
+	_, r, err := conn.NextReader()
+	if err != nil {
+		return err
+	}
+
+	// parse the request
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	var start Timestamp
+	err = start.UnmarshalJSON(bytes.TrimSpace(buf))
+	if err != nil {
+		return err
+	}
+
+	// send the response
+	res := &Response{
+		ID:           host,
+		InitiateTime: start,
+		SendTime:     Timestamp(time.Now()),
+		Leap:         0,
+		Next:         zeroEpochTime,
+		Step:         0,
+	}
+	return conn.WriteJSON(res)
 }
