416e7f911f036985b193338e12074810f9be6f27
Ichinose Shogo <shogo82148@gmail.com>
2017-03-01 00:23:41 +0900

    add more benchmark

--- webntp_test.go
+++ webntp_test.go
@@ -2,6 +2,8 @@ package webntp
 
 import (
 	"bytes"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 )
@@ -129,6 +131,39 @@ func TestParseLeapSecondsList(t *testing.T) {
 
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	s := &Server{
+		LeapSecondsPath: "leap-seconds.list",
+		LeapSecondsURL:  "https://www.ietf.org/timezones/data/leap-seconds.list",
+	}
+	ts := httptest.NewServer(s)
+	defer ts.Close()
+
+	c := &Client{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.Get(ts.URL)
+	}
+}
+
+func BenchmarkGetWS(b *testing.B) {
+	s := &Server{
+		LeapSecondsPath: "leap-seconds.list",
+		LeapSecondsURL:  "https://www.ietf.org/timezones/data/leap-seconds.list",
+	}
+	ts := httptest.NewServer(s)
+	defer ts.Close()
+	u, _ := url.Parse(ts.URL)
+	u.Scheme = "ws"
+	wsURL := u.String()
+
+	c := &Client{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.Get(wsURL)
 	}
 }
 
