ac71cc2ca2ce02084654216e0cfa61d77f508caa
Ichinose Shogo <shogo82148@gmail.com>
2017-03-05 13:02:41 +0900

    add -allow-cross-origin option

--- cmd/webntp/main.go
+++ cmd/webntp/main.go
@@ -6,43 +6,61 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/shogo82148/go-webntp"
 )
 
-func main() {
-	var serveHost string
-	var leapSecondsPath, leapSecondsURL string
-	var samples int
+var serveHost string
+var allowCrossOrigin bool
+var leapSecondsPath, leapSecondsURL string
+var samples int
+
+func init() {
+	// Server options
 	flag.StringVar(&serveHost, "serve", "", "server host name")
+	flag.BoolVar(&allowCrossOrigin, "allow-cross-origin", false, "allow cross origin request")
 	flag.StringVar(&leapSecondsPath, "leap-second-path", "leap-seconds.list", "path for leap-seconds.list cache")
 	flag.StringVar(&leapSecondsURL, "leap-second-url", "https://www.ietf.org/timezones/data/leap-seconds.list", "url for leap-seconds.list")
+
+	// Client options
 	flag.IntVar(&samples, "p", 2, "Specify the number of samples")
+}
+
+func main() {
 	flag.Parse()
 
 	if serveHost != "" {
-		if err := serve(serveHost, leapSecondsPath, leapSecondsURL); err != nil {
+		if err := serve(); err != nil {
 			log.Fatal(err)
 		}
 	} else {
 		if samples < 1 || samples > 8 {
 			log.Fatalf("invalid samples: %d", samples)
 		}
-		if err := client(samples, flag.Args()); err != nil {
+		if err := client(flag.Args()); err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func serve(host, leapSecondsPath, leapSecondsURL string) error {
+func serve() error {
 	s := &webntp.Server{
 		LeapSecondsPath: leapSecondsPath,
 		LeapSecondsURL:  leapSecondsURL,
 	}
+	if allowCrossOrigin {
+		s.Upgrader = &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			Subprotocols:    []string{webntp.Subprotocol},
+			CheckOrigin:     func(*http.Request) bool { return true },
+		}
+	}
 	s.Start()
-	return http.ListenAndServe(host, s)
+	return http.ListenAndServe(serveHost, s)
 }
 
-func client(samples int, hosts []string) error {
+func client(hosts []string) error {
 	c := &webntp.Client{}
 	for _, arg := range hosts {
 		result, err := c.GetMulti(arg, samples)
