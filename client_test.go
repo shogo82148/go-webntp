6c5fb9d977c6df53cff4dc498d118966024fa4e3
Ichinose Shogo <shogo82148@gmail.com>
2019-05-03 12:32:17 +0900

    add test for get via websocket

--- client_test.go
+++ client_test.go
@@ -41,6 +41,43 @@ func TestGet(t *testing.T) {
 	}
 }
 
+func TestGetWebSocket(t *testing.T) {
+	defer func(f func() time.Time) { serverTime = f }(serverTime)
+	serverTime = func() time.Time {
+		return time.Unix(1234567895, 0)
+	}
+
+	defer func(f func() time.Time) { clientStartTime = f }(clientStartTime)
+	clientStartTime = func() time.Time {
+		return time.Unix(1234567890, 0)
+	}
+	defer func(f func() time.Time) { clientEndTime = f }(clientEndTime)
+	clientEndTime = func() time.Time {
+		return time.Unix(1234567892, 0)
+	}
+
+	s := &Server{}
+	s.Start()
+	defer s.Close()
+	ts := httptest.NewServer(s)
+	defer ts.Close()
+
+	u, _ := url.Parse(ts.URL)
+	u.Scheme = "ws"
+	wsURL := u.String()
+	c := &Client{}
+	result, err := c.Get(wsURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.Offset != -4*time.Second {
+		t.Errorf("unexpected offset, want %s, got %s", -4*time.Second, result.Offset)
+	}
+	if result.Delay != 2*time.Second {
+		t.Errorf("unexpected delay, want %s, got %s", 2*time.Second, result.Delay)
+	}
+}
+
 func BenchmarkGet(b *testing.B) {
 	s := &Server{
 		LeapSecondsPath: "leap-seconds.list",
