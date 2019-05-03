d1976c3e2778b23d504673ac12e72f6283bf401b
Ichinose Shogo <shogo82148@gmail.com>
2019-05-03 16:03:00 +0900

    add test for GetMulti

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	crand "crypto/rand"
 	"encoding/binary"
 	"encoding/json"
+	"math/bits"
 	"math/rand"
 	"net/http"
 	"net/http/httptrace"
@@ -57,6 +58,7 @@ func (c *Client) Get(ctx context.Context, uri string) (Result, error) {
 // GetMulti gets synchronization information.
 // It improve accuracy by calling the Get method many times.
 func (c *Client) GetMulti(ctx context.Context, uri string, samples int) (Result, error) {
+	// initialize seed
 	var s int64
 	if err := binary.Read(crand.Reader, binary.LittleEndian, &s); err != nil {
 		s = time.Now().UnixNano()
@@ -74,26 +76,81 @@ func (c *Client) GetMulti(ctx context.Context, uri string, samples int) (Result,
 		if results[i].Delay < minDelay {
 			minDelay = results[i].Delay
 		}
+
+		// sleep a little
 		if i < samples-1 {
 			d := r.Int63n(int64(time.Second))
 			time.Sleep(time.Duration(d))
 		}
 	}
 
-	result := results[len(results)-1]
-	var num int
+	result := results[0]
+	var num int64
+	var delay, offset int128
 	for _, r := range results {
 		if r.Delay >= minDelay*2 {
-			// this sample may be re-sent. ignore it.
+			// the sample of this sample may be re-sent. ignore it.
 			continue
 		}
-		result.Delay += r.Delay
-		result.Offset += r.Offset
+		delay = delay.Add(int64ToInt128(int64(r.Delay)))
+		offset = offset.Add(int64ToInt128(int64(r.Offset)))
 		num++
 	}
-	result.Delay /= time.Duration(num)
-	result.Offset /= time.Duration(num)
+	quo, _ := delay.Div(num)
+	result.Delay = time.Duration(quo)
+	quo, _ = offset.Div(num)
+	result.Offset = time.Duration(quo)
 	return result, nil
+}
+
+type int128 [2]uint64
+
+func int64ToInt128(a int64) int128 {
+	var neg bool
+	if a < 0 {
+		a *= -1
+		neg = true
+	}
+	ret := int128{0, uint64(a)}
+	if neg {
+		ret = ret.Neg()
+	}
+	return ret
+}
+
+// Add returns a + b
+func (a int128) Add(b int128) int128 {
+	sum1, carry := bits.Add64(a[1], b[1], 0)
+	sum0, _ := bits.Add64(a[0], b[0], carry)
+	return int128{sum0, sum1}
+}
+
+func (a int128) Div(b int64) (quo, rem int64) {
+	quoSign := int64(1)
+	remSign := int64(1)
+	if a[1] >= 1<<63 {
+		quoSign *= -1
+		remSign *= -1
+		a = a.Neg()
+	}
+	if b < 0 {
+		quoSign *= -1
+		b *= -1
+	}
+	q, r := bits.Div64(a[0], a[1], uint64(b))
+	quo = int64(q) * quoSign
+	rem = int64(r) * remSign
+	return
+}
+
+func (a int128) Neg() int128 {
+	a[0] = ^a[0]
+	a[1] = ^a[1]
+	a[1]++
+	if a[1] == 0 {
+		a[0]++
+	}
+	return a
 }
 
 func (c *Client) getHTTP(ctx context.Context, uri string) (Result, error) {
